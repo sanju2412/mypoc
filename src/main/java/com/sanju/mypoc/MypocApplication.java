89e44f5fcf0f1e4a919ab190861a5dbf1cbe3aef
Lalit <lalit.dps07@gmail.com>
2019-11-29 00:57:25 +0530

    First commit

---
+++ src/main/java/com/sanju/mypoc/MypocApplication.java
@@ -0,0 +1,13 @@
+package com.sanju.mypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanju/mypoc/MypocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanju.mypoc;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
